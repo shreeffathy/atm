5f36001e24435374b3e4f83c057f8141b673b315
shreeffathy <shreeff1@gmail.com>
2018-09-18 02:58:51 +0300

    create two atm

--- atm.py
+++ atm.py
@@ -1,32 +1,47 @@
-# allowed papers: 100, 50, 10, 5, and rest of request
+class ATM():
+ def __init__(self , balance , bank_name):
+  self.balance=balance
+  self.bank_name=bank_name
+ def withdraw1(self,request):
+  print"========================="
+  print"Welcome to"+self.bank_name
+  print "Current balance"+' '+str(self.balance)
+  print"========================="
+  balance_total=self.balance - request
+  if request > self.balance:
+    print"Can't give you all this money !!"
+  elif request < 0 :
+      print"More than zero plz!"
+  else:
+       while request > 0:
+            if request >= 100:
+                request -= 100
+                print"give 100"
+            elif request >= 50:
+                    request -= 50
+                    print"give 50"
+            elif request >= 10:
+                    request -= 10
+                    print"give 10"
+            elif request >= 5:
+                    request -= 5
+                    print"give 5"
+            elif request >= 2:
+                    request -= 2
+                    print"give 2"
+            else:
+                print"give 1"
+                request -= 1
+  self.balance=balance_total
+        
+balance1 = 500
+balance2 = 1000
 
-money = 500
+atm1 = ATM(balance1,"Smart Bank")
+atm2 = ATM(balance2,"Baraka Bank")
 
-request = 277
-#i=0
+atm1.withdraw1(277)
+atm1.withdraw1(800)
 
-def push_mony(request , money):
- if request > money:
-	print "the request greater than your money"
- else:  
-     while request > 0:
-	        if request >= 100:
-	            print 100
-	            request = request - 100
-	        elif request >= 50 :
-		        print 50
-		        request = request - 50
-	      
-	        elif request >= 10:
-		        print 10
-		        request = request - 10
-	        elif request >= 5:
-		        print 5
-		        request = request - 5
-	        elif request >= 2:
-		        print 2
-		        request = request - 2
-	        else: 
-		        print 1
-		        request = request - 1
-push_mony(request,money)
+atm2.withdraw1(100)
+atm2.withdraw1(2000)
