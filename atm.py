3d1c9c27f3192821edc5ff30cf32213178b9aec9
shreeffathy <shreeff1@gmail.com>
2018-09-22 05:05:53 +0300

    add receipt into atm

--- atm.py
+++ atm.py
@@ -3,6 +3,7 @@ class ATM():
  def __init__(self , balance , bank_name):
   self.balance=balance
   self.bank_name=bank_name
+  self.withdrawals_list = []
   
  def withdraw1(self,request):
   print"========================="
@@ -18,9 +19,22 @@ class ATM():
       for i in ATM.PAPERS:
         while i<=request:
           print("give"+str(i))
+          self.withdrawals_list.append(i)
           request -= i
+      self.withdrawals_list.append("**")
       self.balance=balance_total
-          
+
+ def show_withdrawals(self):
+  sum1=0
+  print("****************************************************")
+  print("**************************RECEIPT*******************")
+  for withdrawal in self.withdrawals_list:
+    print(withdrawal)
+    if withdrawal != "**":
+      sum1 += withdrawal
+  print(" the total balance for withdrawals"+' '+"from"+' '+self.bank_name+' = '+str(sum1))
+
+
 balance1 = 500
 balance2 = 1000
 
@@ -29,6 +43,10 @@ atm2 = ATM(balance2,"Baraka Bank")
 
 atm1.withdraw1(277)
 atm1.withdraw1(800)
+atm1.withdraw1(200)
 
 atm2.withdraw1(100)
 atm2.withdraw1(2000)
+
+atm1.show_withdrawals()
+atm2.show_withdrawals()
